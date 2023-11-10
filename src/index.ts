4102ce9ba55c91f227727720f0d93dd92f5fda60
Nils Alex <nils@famalex.de>
2023-11-10 16:30:15 +0100

    Basic openai-proxy request setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('Hello, world!');
+import { RequestOptions, request } from 'https';
+
+const body = JSON.stringify({
+    model: "gpt-3.5-turbo",
+    messages: [
+        {
+            role: "user",
+            content: "System Instruction: You are a chatbot.\nQuestion: Which name do you like most?\nAnswer:",
+        }
+    ],
+});
+
+const requestOptions: RequestOptions = {
+    host: 'openai-proxy.tngtech.com',
+    path: '/v1/chat/completions',
+    method: 'POST',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Content-Length': body.length,
+    },
+};  
+
+const req = request(requestOptions, (res) => {
+    // console.log(res.statusCode);
+    // console.log(res.headers);
+
+    res.on('data', (d) => process.stdout.write(d));
+});
+
+req.write(body);
+req.end();
+
